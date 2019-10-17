78aa31527fb16c7b3396ddf291caca14ec507aa5
Maras0830 <Maras0830@gmail.com>
2019-10-17 17:58:28 +0800

    refactoring: trait struct

--- src/Exceptions/TransformerException.php
+++ src/Exceptions/TransformerException.php
@@ -1,8 +1,24 @@
 <?php
-namespace Maras0830\LaravelSRT\Exceptions;
+namespace Maras0830\PayNowSDK\Exceptions;
 
 use Exception;
 
 class TransformerException extends Exception
 {
+    /**
+     * @var array
+     */
+    private $required_keys;
+
+    /**
+     * PayNowException constructor.
+     * @param $message
+     * @param array $required_keys
+     */
+    public function __construct($message, $required_keys = [])
+    {
+        parent::__construct("$message:" . implode(',', $required_keys));
+
+        $this->required_keys = $required_keys;
+    }
 }
--- src/Traits/TransformerCollectionTrait.php
+++ src/Traits/TransformerCollectionTrait.php
@@ -9,6 +9,10 @@ trait TransformerCollectionTrait
 {
     protected $required_relation_keys = [];
 
+    protected $missing_required_keys = [];
+
+    protected $is_strict_mode = true;
+
     /**
      * @param array $keys
      * @return $this
@@ -21,42 +25,33 @@ trait TransformerCollectionTrait
     }
 
     /**
+     * is_strict_model
+     * true: TransformerException
+     * false: Log::warning
+     *
+     * @param bool $is_strict_mode
+     */
+    public function setStrictMode($is_strict_mode = true)
+    {
+        $this->is_strict_mode = $is_strict_mode;
+    }
+
+    /**
      * Change laravel collection to Transformer format
      *
      * @param $collection
      * @param TransformerAbstract $transformerAbstract
      * @return mixed
      * @throws TransformerException
+     * @throws \ReflectionException
      */
     public function transCollection($collection, TransformerAbstract $transformerAbstract = null)
     {
-        if (!empty($this->required_relation_keys)) {
-            foreach ($this->required_relation_keys as $required_relation_key) {
-                foreach ($collection as $model) {
-                    if (!$model->relationLoaded($required_relation_key)) {
-                        throw new TransformerException('required relation key not exist');
-                    }
-                }
-            }
-        }
+        $this->setUnRelationLoadedKeys($collection);
 
-        if (is_null($transformerAbstract)) {
-            $vars = get_object_vars($this);
-            $class = new \ReflectionClass($this);
-            $constructor = $class->getConstructor();
+        $this->logStrictMode();
 
-            $parameters = [];
-
-            if ($constructor) {
-                foreach ($constructor->getParameters() as $parameter) {
-                    $parameters[$parameter->getName()] = $vars[$parameter->getName()];
-                }
-            }
-
-            $transformerAbstract = app(get_class($this), $parameters);
-        }
-
-        $collection = $this->collection($collection, $transformerAbstract);
+        $collection = $this->packTransformerData($collection, $transformerAbstract);
 
         $response = app(Manager::class)->createData($collection)->toArray()['data'];
 
@@ -71,11 +66,68 @@ trait TransformerCollectionTrait
      * @param TransformerAbstract $transformerAbstract
      * @param bool $keys
      * @return mixed
+     * @throws TransformerException
+     * @throws \ReflectionException
      */
     public function transCollectionGroup($collection, $group_key, TransformerAbstract $transformerAbstract = null, $keys = false)
     {
         $result = [];
 
+        $this->setUnRelationLoadedKeys($collection);
+
+        $this->logStrictMode();
+
+        $collection = $this->packTransformerData($collection, $transformerAbstract);
+
+        $response = app(Manager::class)->createData($collection)->toArray()['data'];
+
+        foreach ($response as $res) {
+            $result[$res[$group_key]][] = $res;
+        }
+
+        if ($keys) {
+            return $result;
+        } else {
+            return array_values($result);
+        }
+    }
+
+    private function setUnRelationLoadedKeys($collection)
+    {
+        if (!empty($this->required_relation_keys)) {
+            foreach ($this->required_relation_keys as $required_relation_key) {
+                foreach ($collection as $model) {
+                    if (!$model->relationLoaded($required_relation_key) &&
+                        !in_array($required_relation_key, $this->missing_required_keys)) {
+                        $this->missing_required_keys[] = $required_relation_key;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws TransformerException
+     */
+    private function logStrictMode()
+    {
+        if (!empty($this->missing_required_keys)) {
+            if ($this->is_strict_mode) {
+                throw new TransformerException('required relation key not exist', $this->missing_required_keys);
+            } else {
+                Log::warning("required relation key not exist - " . get_class($this), $this->missing_required_keys);
+            }
+        }
+    }
+
+    /**
+     * @param $collection
+     * @param $transformerAbstract
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    private function packTransformerData($collection, $transformerAbstract)
+    {
         if (is_null($transformerAbstract)) {
             $vars = get_object_vars($this);
             $class = new \ReflectionClass($this);
@@ -94,16 +146,6 @@ trait TransformerCollectionTrait
 
         $collection = $this->collection($collection, $transformerAbstract);
 
-        $response = app(Manager::class)->createData($collection)->toArray()['data'];
-
-        foreach ($response as $res) {
-            $result[$res[$group_key]][] = $res;
-        }
-
-        if ($keys) {
-            return $result;
-        } else {
-            return array_values($result);
-        }
+        return $collection;
     }
 }
